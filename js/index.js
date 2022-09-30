710ddd97a8e322e4b4c4138216b66cf8beb5fbb4
Alex <lafak87@gmail.com>
2022-09-30 17:14:02 +0300

    fix: brands 1.5 - 1:slider-card 2:onWindowResize 3:btnToggle

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
 'use strict';
 
-let swiper = {
-  destroyed: true
-};
+addEventListener('DOMContentLoaded', () => {
 
-function onWindowResize () {
-  const clientWidth = document.body.clientWidth;
-
-  if (clientWidth < 768 && swiper.destroyed) {
-    swiper = new Swiper(".swiper", {
-      grabCursor: true,
-      slidesPerView: 1.2,
-      spaceBetween: 15,
-    
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    
-    });
-    return;
-  }
-
-  if (clientWidth >= 768 && !swiper.destroyed) {
-    swiper.destroy();
-    return;
-  }
-};
-
-
-onWindowResize();
-
-window.addEventListener('resize', () => {
+  let swiper;
+  
+  function onWindowResize () {
+    const tablet = window.matchMedia('(min-width: 768px)');
+  
+    if (!tablet.matches) {
+      swiper = new Swiper(".swiper", {
+        grabCursor: true,
+        slidesPerView: 1.2,
+        spaceBetween: 15,
+      
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      
+      });
+      return;
+    }
+  
+    if (tablet.matches && swiper) {
+      swiper.destroy();
+      return;
+    }
+  };
+  
   onWindowResize();
+  
+  window.addEventListener('resize', () => {
+    onWindowResize();
+  });
+  
+  
+  const showBtn = document.querySelector('.content-button');
+  const slider = document.querySelector('.slider');
+  
+  
+  function btnToggle (btn, wrapper) {
+    const btnText = btn.querySelector('span');
+    const btnImg = btn.querySelector('img');
+  
+    btn.addEventListener('click', (e) => {
+      wrapper.classList.toggle('slider--show-all');
+      e.currentTarget.children[1].textContent = wrapper.classList.contains('slider--show-all') ? 'Скрыть' : 'Показать все';
+      e.currentTarget.children[0].classList.toggle('content-button__img--show-all');
+    })
+  };
+  
+  btnToggle(showBtn, slider);
 });
-
-
-const showBtn = document.querySelector('.content-button');
-const slider = document.querySelector('.slider');
-
-
-function btnToggle (btn, wrapper) {
-  const btnText = btn.querySelector('span');
-  const btnImg = btn.querySelector('img');
-
-  btn.addEventListener('click', () => {
-    wrapper.classList.toggle('slider--show-all');
-    btnText.textContent = slider.classList.contains('slider--show-all') ? 'Скрыть' : 'Показать все';
-    btnImg.classList.toggle('content-button__img--show-all');
-  })
-};
-
-btnToggle(showBtn, slider);
